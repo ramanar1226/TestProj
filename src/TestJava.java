11ce6efda8bd2889ed15ea0bc63852af371f7acf
ramana <reddy@gmail.com>
2020-06-14 14:15:35 +0530

    stash latest

--- src/TestJava.java
+++ src/TestJava.java
@@ -4,6 +4,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("stash changes");
+		System.out.println("stash changes latest");
 	}
 
 }
